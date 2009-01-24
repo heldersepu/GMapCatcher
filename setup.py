b0828858545a7509a508b8782b6d53f12dde1bed
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-01-24 15:35:06 +0000

    Initial commit of ALL setup files
    we might be able to reduce the size:
    http://py2exe.org/index.cgi/OptimizingSize

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    name = 'gmapcatcher',
+    description = 'Offline Google Map Viewer',
+    version = '0.041',
+
+    windows = [
+                  {
+                      'script': 'maps.py',
+                      'icon_resources': [(1, "maps.ico")],
+                  }
+              ],
+
+    options = {
+                  'py2exe': {
+                      'packages':'encodings',
+                      'includes': 'cairo, pango, pangocairo, atk, gobject',
+                  }
+              },
+
+    data_files=[
+                   'Changelog',
+                   'README'
+               ]
+)
