f8492e83cf7a89ce3ef1243d4ed0504f6d501a67
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-02-03 02:01:18 +0000

    Inc Version #

--- trunk/setup.py
+++ trunk/setup.py
@@ -4,7 +4,7 @@ import py2exe
 setup(
     name = 'gmapcatcher',
     description = 'Offline Google Map Viewer',
-    version = '0.043',
+    version = '0.044',
 
     windows = [
                   {
