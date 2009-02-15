e48b3149ac056b65776fe9b426f50b8a690129e3
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-02-15 22:37:00 +0000

    v 0.048  * Add description in the query entry box  * Add some info in the missing.png image  * Double-Click functionality in the Map

--- trunk/setup.py
+++ trunk/setup.py
@@ -4,7 +4,7 @@ import py2exe
 setup(
     name = 'gmapcatcher',
     description = 'Offline Google Map Viewer',
-    version = '0.047',
+    version = '0.048',
     url = "http://code.google.com/p/gmapcatcher/",
 
     windows = [
