642ea102a83751369e8c6fff025b3f7345bfadee
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-02-17 17:05:12 +0000

    Split maps.py into (mapUtils.py), move all the "threading" into mapUtils

--- maps.py
+++ maps.py
@@ -5,39 +5,7 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 import googleMaps
-import threading
-from threading import Thread
-
-class GetTileThread(Thread):
-        def __init__(self,  zl,
-                        real_tile_x, real_tile_y,
-                        tile_x_pos_inner, tile_y_pos_inner,
-                        x_pos, y_pos,
-                        draw_width, draw_height,
-                        online, force_update, gc, window):
-                Thread.__init__(self)
-                self.zl = zl
-                self.x = real_tile_x
-                self.y = real_tile_y
-                self.xp = x_pos
-                self.yp = y_pos
-                self.draw_width = draw_width
-                self.draw_height = draw_height
-                self.online = online
-                self.force_update = force_update
-                self.gc = gc
-                self.window = window
-                self.xi = tile_x_pos_inner
-                self.yi = tile_y_pos_inner
-
-        def run(self):
-                pixbuf = self.window.ctx_map.get_tile_pixbuf(self.zl, (self.x, self.y), self.online, self.force_update)
-                gc = self.gc
-                self.window.drawing_area.window.draw_pixbuf(gc, pixbuf,
-                                                        int(self.xi), int(self.yi),
-                                                        int(self.xp), int(self.yp),
-                                                        int(self.draw_width), int(self.draw_height))
-                return
+import mapUtils
 
 class MainWindow(gtk.Window):
         center = ((0,0),(128,128))
@@ -302,55 +270,11 @@ class MainWindow(gtk.Window):
         def expose_cb(self, drawing_area, event):
                 online = not self.cb_offline.get_active()
                 force_update = self.cb_forceupdate.get_active()
-                area = event.area
-                #print "expose_cb: area x=%d, y=%d, width=%d, height=%d" % (area.x, area.y, area.width, area.height)
                 rect = drawing_area.get_allocation()
-                #print "canvas: x=%d, y=%d, w=%d, h=%d" % (rect.x, rect.y, rect.width, rect.height)
-
-                tl_point = (self.center[1][0] - rect.width / 2,
-                            self.center[1][1] - rect.height / 2)
-                #print "tl_point: (%d,%d)" % tl_point
                 zl = self.get_zoom_level()
-                tl_tile, tl_offset = self.ctx_map.tile_adjustEx(zl, self.center[0],
-                                tl_point)
-
-                y_pos = 0
-                tile_y_pos = tl_tile[1]
-                tile_y_pos_inner = tl_offset[1]
-                draw_height = googleMaps.TILES_HEIGHT - tl_offset[1]
-                threads = []
-                while (y_pos < rect.height):
-                        tile_x_pos = tl_tile[0]
-                        tile_x_pos_inner = tl_offset[0]
-                        draw_width = googleMaps.TILES_WIDTH - tl_offset[0]
-                        x_pos = 0
-                        while (x_pos < rect.width):
-                        #############################################
-                                real_tile_x, real_tile_y = self.ctx_map.tile_adjust(self.get_zoom_level(),
-                                                (tile_x_pos, tile_y_pos))
-                                if not (((area.x + area.width < x_pos) or (x_pos + draw_width < area.x)) or \
-                                                ((area.y + area.height < y_pos) or (y_pos + draw_height < area.y))):
-                                        th = GetTileThread(zl, real_tile_x, real_tile_y, tile_x_pos_inner,
-                                                tile_y_pos_inner, x_pos, y_pos, draw_width, draw_height, online,
-                                                force_update, self.drawing_area.style.black_gc, self)
-                                        threads.append(th)
-                                        th.start()
-
-                                x_pos += draw_width
-                                tile_x_pos += 1
-                                tile_x_pos_inner = 0
-                                draw_width = googleMaps.TILES_WIDTH
-                                if (x_pos + draw_width > rect.width):
-                                        draw_width = rect.width - x_pos
-
-                        y_pos += draw_height
-                        tile_y_pos += 1
-                        tile_y_pos_inner = 0
-                        draw_height = googleMaps.TILES_HEIGHT
-                        if (y_pos + draw_height > rect.height):
-                                draw_height = rect.height - y_pos
-                for th in threads:
-                        th.join()
+                mapUtils.do_expose_cb(self, zl, self.center, rect, online, force_update, \
+                                      self.drawing_area.style.black_gc, event.area, \
+                                      googleMaps.TILES_HEIGHT, googleMaps.TILES_WIDTH)
 
         def scroll_cb(self, widget, event):
                 if (event.direction == gtk.gdk.SCROLL_UP):
