0738bcc774f8cb0a9d991107f1de93be1cbeca89
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-04-01 13:55:13 +0000

    Some details in the batch script that builds the windows installer

--- trunk/setup.bat
+++ trunk/setup.bat
@@ -26,12 +26,13 @@ C:\PYTHON26\PYTHON.EXE setup.py py2exe
 @XCOPY /E common\* dist 
 
 :: Launch the NSIS setup
-@COLOR 07
+@COLOR 0F
 "%programfiles%\NSIS\makensis.exe" setup.nsi
 
+@COLOR 0A
 @Echo.
 @PAUSE
+
 :: clean up at the end
 @RD build /s /q
 @RD dist /s /q
-
