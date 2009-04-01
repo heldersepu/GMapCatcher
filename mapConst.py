5629ea209aa6b53e009d69492ad58569504c0699
Maxim.Razin <Maxim.Razin@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-04-01 08:09:47 +0000

    svn merge -r237:253 https://gmapcatcher.googlecode.com/svn/branches/mvr-current

--- mapConst.py
+++ mapConst.py
@@ -7,3 +7,9 @@ TILES_HEIGHT = 256
 NR_MTS = 4
 SEPARATOR = "\t"
 TOOLS_MENU = ["Settings", "Edit locations", "Edit markers", "Change Theme"]
+
+LAYER_MAP = 0
+LAYER_SATELLITE = 1
+LAYER_TERRAIN = 2
+LAYER_NAMES = ["Map", "Satellite", "Terrain"]
+LAYER_DIRS = ["tiles", "sat_tiles", "ter_tiles"]
