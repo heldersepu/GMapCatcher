8067343cc1677bbf4854f4fcdf9131aa2040bfea
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-04-09 16:16:58 +0000

    Bring the latest changes from the trunk

--- branches/mvr-current/mapTools.py
+++ branches/mvr-current/mapTools.py
@@ -6,15 +6,25 @@ from mapConst import *
 
 class TreeView():
 
-    def __read_file(self, strFilePath):
-        return fileUtils.read_file('location',strFilePath)
+    # Appends items to a list from the given file
+    def __read_file(self, strInfo, strFilePath, liststore):
+        locations = fileUtils.read_file(strInfo, strFilePath)
+        # add rows with text
+        for strLoc in locations.keys():
+            liststore.append([strLoc , locations[strLoc][0],
+                              locations[strLoc][1], locations[strLoc][2]])
+        return liststore
 
-    def __write_file(self, strFilePath):
+    # Writes a given list to the file
+    def __write_file(self, strInfo, strFilePath, liststore):
         locations = {}
-        for row in self.liststore:
+        for row in liststore:
+            print row[0]
             locations[row[0]] = (float(row[1]), float(row[2]), int(row[3]))
-        fileUtils.write_file('location',strFilePath, locations)
+        print strFilePath, strInfo
+        fileUtils.write_file(strInfo, strFilePath, locations)
 
+    # Handle the 'edited' event of the cells
     def __cell_edited(self, cell, row, new_text, model, col):
         try:
             if col == 0:
@@ -26,19 +36,77 @@ class TreeView():
         except Exception:
             pass
 
-    def show(self, filePath):
-        locations = self.__read_file(filePath)
+    # Change the selection
+    def change_selection(self, myTree, intPath):
+        if intPath >= 0:
+            myTree.set_cursor(intPath)
+            myTree.grab_focus()
 
+    # Add a row to the list
+    def btn_add_clicked(self, button, liststore, myTree):
+        iter = liststore.append([' New', 0, 0, MAP_MAX_ZOOM_LEVEL - 2])
+        liststore.set_sort_column_id(0, gtk.SORT_ASCENDING)
+        self.change_selection(myTree, liststore.get_path(iter))
+
+    # Remove selected row from the list
+    def btn_remove_clicked(self, button, liststore, myTree):
+        treeSelection = myTree.get_selection()
+        model, iter = treeSelection.get_selected()
+        if iter:
+            intPath = liststore.get_path(iter)
+            liststore.remove(iter)
+            if model.get_iter_first():
+                self.change_selection(myTree, intPath)
+
+    # Reload the list from the file
+    def btn_revert_clicked(self, button, strInfo, filePath, liststore, myTree):
+        liststore.clear()
+        myTree.set_model(self.__read_file(strInfo, filePath, liststore))
+        liststore.set_sort_column_id(0, gtk.SORT_ASCENDING)
+        myTree.grab_focus()
+
+    # Save the curent list to the file
+    def btn_save_clicked(self, button, strInfo, filePath, liststore):
+        self.__write_file(strInfo, filePath, liststore)
+
+    # All the buttons below the list
+    def __action_buttons(self, strInfo, filePath, liststore, myTree):
+        bbox = gtk.HButtonBox()
+        bbox.set_border_width(10)
+
+        button = gtk.Button(stock=gtk.STOCK_ADD)
+        button.connect('clicked', self.btn_add_clicked, liststore, myTree)
+        bbox.add(button)
+
+        gtk.stock_add([(gtk.STOCK_REMOVE, "_Delete", 0, 0, "")])
+        button = gtk.Button(stock=gtk.STOCK_REMOVE)
+        button.connect('clicked', self.btn_remove_clicked, liststore, myTree)
+        bbox.add(button)
+
+        button = gtk.Button(stock=gtk.STOCK_REVERT_TO_SAVED)
+        button.connect('clicked', self.btn_revert_clicked,
+                        strInfo, filePath, liststore, myTree)
+        bbox.add(button)
+
+        button = gtk.Button(stock=gtk.STOCK_SAVE)
+        button.connect('clicked', self.btn_save_clicked,
+                        strInfo, filePath, liststore)
+        bbox.add(button)
+        return bbox
+
+    # Handle the delete key
+    def key_press_tree(self, w, event, liststore):
+        if event.keyval == 65535:
+            self.btn_remove_clicked(None, liststore, w)
+            return True
+
+    def show(self, strInfo, filePath):
         # create a liststore with one string column to use as the model
-        self.liststore = gtk.ListStore(str, float, float, int)
+        liststore = gtk.ListStore(str, float, float, int)
 
         # create the TreeView using liststore
-        myTree = gtk.TreeView(self.liststore)
-
-        # add rows with text
-        for strLoc in locations.keys():
-            self.liststore.append([strLoc , locations[strLoc][0],
-                                locations[strLoc][1], locations[strLoc][2]])
+        myTree = gtk.TreeView(self.__read_file(strInfo, filePath, liststore))
+        myTree.connect("key-press-event", self.key_press_tree, liststore)
 
         strCols = ['Location', 'Latitude', 'Longitude', 'Zoom']
         for intPos in range(len(strCols)):
@@ -46,21 +114,32 @@ class TreeView():
             cell = gtk.CellRendererText()
             # Allow Cells to be editable
             cell.set_property('editable', True)
-            cell.connect('edited', self.__cell_edited, self.liststore, intPos)
+            cell.connect('edited', self.__cell_edited, liststore, intPos)
             # Create the TreeViewColumns to display the data
             tvcolumn = gtk.TreeViewColumn(strCols[intPos])
             myTree.append_column(tvcolumn)
             tvcolumn.pack_start(cell, True)
             tvcolumn.set_attributes(cell, text=intPos)
             tvcolumn.set_sort_column_id(intPos)
+            tvcolumn.set_resizable(True)
+            if intPos == 0:
+                tvcolumn.set_expand(True)
+            else:
+                tvcolumn.set_min_width(75)
 
         # make myTree searchable by location
         myTree.set_search_column(0)
-        self.liststore.set_sort_column_id(0, gtk.SORT_ASCENDING)
+        liststore.set_sort_column_id(0, gtk.SORT_ASCENDING)
 
+        hpaned = gtk.VPaned()
         scrolledwindow = gtk.ScrolledWindow()
+        scrolledwindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         scrolledwindow.add(myTree)
-        return scrolledwindow
+        hpaned.pack1(scrolledwindow, True, True)
+
+        buttons = self.__action_buttons(strInfo, filePath, liststore, myTree)
+        hpaned.pack2(buttons, False, False)
+        return hpaned
 
 class MainWindow:
 
@@ -79,7 +158,7 @@ class MainWindow:
             frame.set_border_width(10)
             frame.set_size_request(100, 75)
             if str in [TOOLS_MENU[1], TOOLS_MENU[2]]:
-                frame.add(myTree.show(filePath +'/'+ str[5:]))
+                frame.add(myTree.show(str[5:-1], filePath +'/'+ str[5:]))
             else:
                 frame.add(gtk.Label(str + ' coming soon!! '))
             label = gtk.Label(str)
@@ -92,8 +171,8 @@ class MainWindow:
         win.set_border_width(10)
         win.set_transient_for(parent)
         win.set_size_request(600, 400)
+        win.set_destroy_with_parent(True)
         win.set_title(" GMapCatcher Tools ")
-        win.connect("delete_event", gtk.main_quit)
 
         myNotebook = self.__create_notebook(configpath)
         win.add(myNotebook)
@@ -102,7 +181,6 @@ class MainWindow:
 
 def main(parent, configpath, start_page):
     MainWindow(parent, configpath, start_page)
-    gtk.main()
 
 if __name__ == "__main__":
     main()
