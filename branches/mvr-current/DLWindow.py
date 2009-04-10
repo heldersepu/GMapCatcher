7d77bb55329c7ffcd92026cf8952df09e69a0ff2
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-04-10 13:50:35 +0000

    Merge functionality that was added directly to the Trunk

--- branches/mvr-current/DLWindow.py
+++ branches/mvr-current/DLWindow.py
@@ -19,71 +19,148 @@ def nice_round(f):
     return p*10**n
 
 class DLWindow(gtk.Window):
-    def __init__(self, coord, kmx,kmy, layer):
+    def __init__(self, coord, kmx, kmy, layer):
+        # Validate the text on all the "input" widgets
+        def insert_text(entry, text, length, position, max, isInt=True):
+            # generate what the new text will be
+            text = text[:length]
+            pos = entry.get_position()
+            old = entry.get_text()
+            new_text = old[:pos] + text + old[pos:]
+
+            # Allow the minus as the first char
+            if new_text == "-":
+                return
+            # Allow a maximum number of chars
+            elif (len(new_text) > max):
+                entry.stop_emission('insert-text')
+            else:
+                # If an exception is raised the text is not good
+                try:
+                    if isInt:
+                        int(new_text)
+                    else:
+                        float(new_text)
+                except Exception:
+                    entry.stop_emission('insert-text')
+
         def lbl(text):
-            l=gtk.Label(text)
+            l = gtk.Label(text)
             l.set_justify(gtk.JUSTIFY_RIGHT)
             return l
-        print "DLWindow(",coord,kmx,kmy,layer,')'
-        kmx=nice_round(kmx)
-        kmy=nice_round(kmy)
-        self.layer=layer
+
+        def _frame(strName, container, spacing = 5):
+            frame = gtk.Frame(strName)
+            vbox = gtk.VBox(False, spacing)
+            vbox.set_border_width(spacing)
+            vbox.pack_start(container)
+            frame.add(vbox)
+            return frame
+
+        def _zoom(zoom0, zoom1):
+            out_hbox = gtk.HBox(False, 50)
+            out_hbox.set_border_width(10)
+            in_hbox = gtk.HBox(False, 20)
+            in_hbox.pack_start(lbl("min:"), False)
+            a_zoom0 = gtk.Adjustment(zoom0, MAP_MIN_ZOOM_LEVEL,
+                                     MAP_MAX_ZOOM_LEVEL, 1)
+            self.s_zoom0 = gtk.SpinButton(a_zoom0)
+            self.s_zoom0.connect('insert-text', insert_text, 2)
+            self.s_zoom0.set_digits(0)
+            in_hbox.pack_start(self.s_zoom0)
+            out_hbox.pack_start(in_hbox)
+
+            in_hbox = gtk.HBox(False, 20)
+            in_hbox.pack_start(lbl("max:"), False)
+            a_zoom1 = gtk.Adjustment(zoom1, MAP_MIN_ZOOM_LEVEL,
+                                     MAP_MAX_ZOOM_LEVEL, 1)
+            self.s_zoom1 = gtk.SpinButton(a_zoom1)
+            self.s_zoom1.connect('insert-text', insert_text, 2)
+            self.s_zoom1.set_digits(0)
+            in_hbox.pack_start(self.s_zoom1)
+            out_hbox.pack_start(in_hbox)
+            hbox = gtk.HBox()
+            hbox.set_border_width(10)
+            hbox.pack_start(_frame(" Zoom ", out_hbox, 0))
+            return hbox
+
+        def _center(lat0, lon0):
+            vbox = gtk.VBox(False, 5)
+            hbox = gtk.HBox(False, 10)
+            hbox.pack_start(lbl("latitude:"))
+            self.e_lat0 = gtk.Entry()
+            self.e_lat0.set_text("%.6f" % lat0)
+            self.e_lat0.connect('insert-text', insert_text, 15, False)
+            hbox.pack_start(self.e_lat0, False)
+            vbox.pack_start(hbox)
+
+            hbox = gtk.HBox(False, 10)
+            hbox.pack_start(lbl("longitude:"))
+            self.e_lon0 = gtk.Entry()
+            self.e_lon0.set_text("%.6f" % lon0)
+            self.e_lon0.connect('insert-text', insert_text, 15, False)
+            hbox.pack_start(self.e_lon0, False)
+            vbox.pack_start(hbox)
+            return _frame(" Center ", vbox)
+
+        def _area(kmx, kmy):
+            vbox = gtk.VBox(False, 5)
+            hbox = gtk.HBox(False, 10)
+            hbox.pack_start(lbl("width:"))
+            self.e_kmx = gtk.Entry()
+            self.e_kmx.set_text("%.6g" % kmx)
+            self.e_kmx.connect('insert-text', insert_text, 10)
+            hbox.pack_start(self.e_kmx, False)
+            vbox.pack_start(hbox)
+
+            hbox = gtk.HBox(False, 10)
+            hbox.pack_start(lbl("height:"))
+            self.e_kmy = gtk.Entry()
+            self.e_kmy.set_text("%.6g" % kmy)
+            self.e_kmy.connect('insert-text', insert_text, 10)
+            hbox.pack_start(self.e_kmy, False)
+            vbox.pack_start(hbox)
+            return _frame(" Area (km) ", vbox)
+
+        def _buttons():
+            hbbox = gtk.HButtonBox()
+            hbbox.set_border_width(10)
+            hbbox.set_layout(gtk.BUTTONBOX_SPREAD)
+            self.b_download = gtk.Button(stock=gtk.STOCK_HARDDISK)
+            self.b_download.connect('clicked', self.run)
+            hbbox.pack_start(self.b_download)
+
+            self.b_cancel = gtk.Button(stock='gtk-cancel')
+            self.b_cancel.connect('clicked', self.cancel)
+            self.b_cancel.set_sensitive(False)
+            hbbox.pack_start(self.b_cancel)
+            return hbbox
+
+        print "DLWindow(", coord, kmx, kmy, layer, ')'
+        kmx = nice_round(kmx)
+        kmy = nice_round(kmy)
+        self.layer = layer
         gtk.Window.__init__(self)
-        lat0=coord[0]
-        lon0=coord[1]
-        zoom0=max(MAP_MIN_ZOOM_LEVEL,coord[2]-3)
-        zoom1=min(MAP_MAX_ZOOM_LEVEL,coord[2]+1)
+        lat0 = coord[0]
+        lon0 = coord[1]
+        zoom0 = max(MAP_MIN_ZOOM_LEVEL, coord[2]-3)
+        zoom1 = min(MAP_MAX_ZOOM_LEVEL, coord[2]+1)
 
-        tbl=gtk.Table(rows=4, columns=4, homogeneous=False)
-        tbl.set_col_spacings(10)
-        tbl.set_row_spacings(10)
+        vbox = gtk.VBox(False)
+        hbox = gtk.HBox(False, 10)
+        hbox.pack_start(_center(lat0, lon0))
+        hbox.pack_start(_area(kmx, kmy))
+        vbox.pack_start(hbox)
+        vbox.pack_start(_zoom(zoom0, zoom1))
+        vbox.pack_start(_buttons())
 
-        tbl.attach(lbl("Center latitude:"),0,1,0,1)
-        self.e_lat0=gtk.Entry()
-        self.e_lat0.set_text("%.6f" % lat0)
-        tbl.attach(self.e_lat0, 1,2,0,1)
-        tbl.attach(lbl("longitude:"),2,3,0,1)
-        self.e_lon0=gtk.Entry()
-        self.e_lon0.set_text("%.6f" % lon0)
-        tbl.attach(self.e_lon0, 3,4,0,1)
-
-        tbl.attach(lbl("Area width (km):"),0,1,1,2)
-        self.e_kmx=gtk.Entry()
-        self.e_kmx.set_text("%.6g" % kmx)
-        tbl.attach(self.e_kmx, 1,2,1,2)
-        tbl.attach(lbl("Area height (km):"),2,3,1,2)
-        self.e_kmy=gtk.Entry()
-        self.e_kmy.set_text("%.6g" % kmy)
-        tbl.attach(self.e_kmy, 3,4,1,2)
-
-        tbl.attach(lbl("Zoom min:"),0,1,2,3)
-        a_zoom0=gtk.Adjustment(zoom0,MAP_MIN_ZOOM_LEVEL,MAP_MAX_ZOOM_LEVEL,1)
-        self.s_zoom0=gtk.SpinButton(a_zoom0)
-        self.s_zoom0.set_digits(0)
-        tbl.attach(self.s_zoom0, 1,2,2,3)
-        tbl.attach(lbl("max:"),2,3,2,3)
-        a_zoom1=gtk.Adjustment(zoom1,MAP_MIN_ZOOM_LEVEL,MAP_MAX_ZOOM_LEVEL,1)
-        self.s_zoom1=gtk.SpinButton(a_zoom1)
-        self.s_zoom1.set_digits(0)
-        tbl.attach(self.s_zoom1, 3,4,2,3)
-
-        self.b_download=gtk.Button(label="Download")
-        tbl.attach(self.b_download, 1,2,3,4, xoptions=gtk.EXPAND|gtk.FILL, yoptions=0)
-        self.b_download.connect('clicked', self.run)
-
-        self.b_cancel=gtk.Button(stock='gtk-cancel')
-        tbl.attach(self.b_cancel, 3,4,3,4, xoptions=gtk.EXPAND|gtk.FILL, yoptions=0)
-        self.b_cancel.connect('clicked', self.cancel)
-        self.b_cancel.set_sensitive(False)
-
-        self.pbar=gtk.ProgressBar()
-        tbl.attach(self.pbar, 0,4,4,5, xoptions=gtk.EXPAND|gtk.FILL, yoptions=0)
-
-        self.add(tbl)
+        self.pbar = gtk.ProgressBar()
+        self.pbar.set_text(" ")
+        vbox.pack_start(self.pbar)
+        self.add(vbox)
 
         self.set_title("GMapCatcher download")
         self.set_border_width(10)
-        self.set_size_request(600, 300)
 
         self.complete=[]
         self.processing=False
@@ -95,18 +172,19 @@ class DLWindow(gtk.Window):
     def run(self,w):
         if self.processing: return
         try:
-            lat0=float(self.e_lat0.get_text())
-            lon0=float(self.e_lon0.get_text())
-            kmx=float(self.e_kmx.get_text())
-            kmy=float(self.e_kmy.get_text())
-            zoom0=self.s_zoom0.get_value_as_int()
-            zoom1=self.s_zoom1.get_value_as_int()
-            layer=self.layer
+            lat0 = float(self.e_lat0.get_text())
+            lon0 = float(self.e_lon0.get_text())
+            kmx = float(self.e_kmx.get_text())
+            kmy = float(self.e_kmy.get_text())
+            zoom0 = self.s_zoom0.get_value_as_int()
+            zoom1 = self.s_zoom1.get_value_as_int()
+            layer = self.layer
         except ValueError:
-            d=gtk.MessageDialog(self,gtk.DIALOG_MODAL,gtk.MESSAGE_ERROR,gtk.BUTTONS_CLOSE,
-                "Some field contain non-numbers")
+            d = gtk.MessageDialog(self, gtk.DIALOG_MODAL, gtk.MESSAGE_ERROR,
+                    gtk.BUTTONS_CLOSE, "Some field contain non-numbers")
             d.run()
             d.destroy()
+            return
         self.b_cancel.set_sensitive(True)
         self.b_download.set_sensitive(False)
         print ("lat0=%g lon0=%g kmx=%g kmy=%g zoom0=%d zoom1=%d layer=%d"
@@ -118,7 +196,7 @@ class DLWindow(gtk.Window):
         self.downloader=MapDownloader(self.gmap)
         if zoom0>zoom1:
             zoom0,zoom1=zoom1,zoom0
-	self.all_placed=False
+        self.all_placed=False
         self.processing=True
         for zoom in xrange(zoom1, zoom0-1,-1):
             self.downloader.query_region_around_location(
@@ -127,7 +205,7 @@ class DLWindow(gtk.Window):
                 gui_callback(self.tile_received))
         if self.downloader.qsize()==0:
             self.download_complete()
-	self.all_placed=True
+        self.all_placed=True
 
     def tile_received(self, coord, layer, filename):
         print "tile_received(", coord, layer, self.processing, self.downloader!=None,")"
