40e0933ba498d5f2400a88b6642196d3313d86f2
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-06-07 14:54:51 +0000

    Improve reading the config file

--- branches/mvr-current/mapConf.py
+++ branches/mvr-current/mapConf.py
@@ -4,6 +4,7 @@ import mapUtils
 import fileUtils
 from mapConst import *
 
+## Class used to read and save the configuration values
 class MapConf():
 
     init_path = None
@@ -12,6 +13,7 @@ class MapConf():
     init_zoom = MAP_MAX_ZOOM_LEVEL
     init_center = ((0,0),(128,128))
 
+    ## Returns the Path to the configuration file
     def get_configpath(self):
         # the config file most be in the DEFAULT_PATH
         configpath = os.path.expanduser(DEFAULT_PATH)
@@ -19,6 +21,8 @@ class MapConf():
         configpath = os.path.join(configpath, 'gmapcatcher.conf')
         return configpath
 
+    ## Initialise all variables
+    #  If the file does not exit it will be created
     def __init__(self):
         configpath = self.get_configpath()
         if os.path.exists(configpath):
@@ -26,6 +30,7 @@ class MapConf():
         else:
             self.write(configpath)
 
+    ## Write the configuration to the given file
     def write(self, configpath):
         config = ConfigParser.RawConfigParser()
         strSection = 'init'
@@ -40,6 +45,7 @@ class MapConf():
         configfile = open(configpath, 'wb')
         config.write(configfile)
 
+    ## Reads from a given file the configuration
     def read(self, configpath):
         config = ConfigParser.RawConfigParser()
         config.read(configpath)
@@ -51,15 +57,19 @@ class MapConf():
             strCenter = config.get(strSection, 'center')
             self.init_center = mapUtils.str_to_tuple(strCenter)
             self.init_path = config.get(strSection, 'path')
-            # Check directory; If it does not exists try to create it.
-            if not os.path.isdir(self.init_path):
-                try:
-                    os.mkdir(self.init_path)
-                except Exception:
-                    self.init_path = None
+            if self.init_path.strip().lower() in ['none', '']:
+                self.init_path = None
+            else:
+                # Check directory; If it does not exists try to create it.
+                if not os.path.isdir(self.init_path):
+                    try:
+                        os.mkdir(self.init_path)
+                    except Exception:
+                        self.init_path = None
         except Exception:
             pass
 
+    ## Write the configuration to the default file
     def save(self):
         configpath = self.get_configpath()
         self.write(configpath)
