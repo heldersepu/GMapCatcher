92c52bbc3c2598348b2832093a8d9d6806878f2b
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-07-06 01:48:17 +0000

    Improve the Cross image in case of errors

--- branches/mvr-current/mapPixbuf.py
+++ branches/mvr-current/mapPixbuf.py
@@ -1,10 +1,11 @@
 ## @package mapPixBuf
-# Get pixbuf from files
+# Get the Pixbuf from image files.
+#
 
 import gtk
 from mapConst import *
 
-
+## Get the Pixbuf from missing.png
 def missing():
     try:
         pix_missing = gtk.gdk.pixbuf_new_from_file('missing.png')
@@ -14,11 +15,15 @@ def missing():
             TILES_WIDTH, TILES_HEIGHT, TILES_HEIGHT * 3)
     return pix_missing
 
+## Get the Pixbuf from cross.png
 def cross():
     try:
         pix_cross = gtk.gdk.pixbuf_new_from_file('cross.png')
     except Exception:
-        pix_cross = gtk.gdk.pixbuf_new_from_data('\255\255\255' * 1000,
+        pix_cross = gtk.gdk.pixbuf_new_from_data(
+            ('\255\255\255' * 4 + '\0\0\255' * 4 + '\255\255\255' * 4) * 4 +
+            ('\0\0\255' * 12) * 4 +
+            ('\255\255\255' * 4 + '\0\0\255' * 4 + '\255\255\255' * 4) * 4,
             gtk.gdk.COLORSPACE_RGB, False, 8, 12, 12, 12 * 3)
     return pix_cross
 
