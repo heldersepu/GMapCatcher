e8ff05580cb5ad60774ebfe4d48150a1c8c85a96
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-07-06 02:10:46 +0000

    Revert setup.bat to previous revision

--- branches/mvr-current/setup.bat
+++ branches/mvr-current/setup.bat
@@ -14,7 +14,7 @@ C:\PYTHON26\PYTHON.EXE setup.py py2exe
 :: Few seconds delay to show dependencies
 @COLOR F0
 @ECHO.
-::@PING 1.1 > NUL
+@PING 1.1 > NUL
 @COLOR 03
 
 :: Copy individual files
@@ -27,7 +27,7 @@ C:\PYTHON26\PYTHON.EXE setup.py py2exe
 
 :: Launch the NSIS setup
 @COLOR 07
-"C:\Program Files (x86)\NSIS\makensis.exe" setup.nsi
+"%programfiles%\NSIS\makensis.exe" setup.nsi
 
 @Echo.
 @PAUSE
