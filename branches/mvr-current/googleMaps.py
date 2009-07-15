471482e4236a0de14b9fefb3af7071718ec1e663
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-07-15 13:23:14 +0000

    Show the OpenStreetMap only in the "map layer"

--- branches/mvr-current/googleMaps.py
+++ branches/mvr-current/googleMaps.py
@@ -138,10 +138,12 @@ class GoogleMaps:
         else:
             return 'error=Unable to get latitude and longitude of %s ' % location
 
+    ## Get the URL for the given coordinates
+    # In this function we point to the map service
     def get_url_from_coord(self, coord, layer, online):
         self.mt_counter += 1
         self.mt_counter = self.mt_counter % NR_MTS
-        if self.mapService == 'OpenStreetMap':
+        if self.mapService == 'OpenStreetMap' and (layer == LAYER_MAP):
             return openStreetMaps.get_url(self.mt_counter, coord)
         else:
             return self.get_url(self.mt_counter, coord, layer, online)
