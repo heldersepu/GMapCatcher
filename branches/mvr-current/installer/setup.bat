d5477a41f7be214f8742773ce30f1f087831109a
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-07-16 18:34:47 +0000

    Add dependencies

--- branches/mvr-current/installer/setup.bat
+++ branches/mvr-current/installer/setup.bat
@@ -1,6 +1,18 @@
 :: Create an executable using py2exe then
 :: Make the installer using NSIS
 
+:: Tested with:
+::  python-2.6.2.msi
+::  gtk2-runtime-2.14.7-2009-01-13-ash.exe
+::  gtk2-themes-2008-10-22-ash.exe
+::  py2exe-0.6.9.win32-py2.6.exe
+::  pycairo-1.4.12-2.win32-py2.6.exe
+::  pygobject-2.14.2-2.win32-py2.6.exe
+::  pygtk-2.12.1-3.win32-py2.6.exe
+::  nsis-2.45-setup.exe
+::  AccessControl.zip
+
+
 @COLOR 02
 :: clean up before starting
 @DEL *.exe
