2f3c15b898be2316edac2ac71e552c67d5aea93c
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-08-17 23:07:32 +0000

    Add a bash script to build the tar.gz file

---
+++ installer/build_gz.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This Script is used to build the tar.gz file
+
+cd ..
+
+dVer=''
+dName=''
+dFile='cat src\mapConst.py'
+
+for dLine in $dFile
+do
+    if [$dLine = 'NAME =']
+    then
+        dName=''
+    else
+        if [$dLine = 'VERSION =']
+        then
+            dVer=''
+        fi
+    fi
+
+    # if [dName!='' && dVer='']
+    # then
+        # # Exit For
+    # fi
+done
+
+tar czf ../GMapCatcher-0.1.1.0.tar.gz *.py src images installer Changelog README
