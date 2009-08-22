e011b8328ea38bbb3f54b4e4bbb9bfd98947d141
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-08-22 18:41:43 +0000

    Continue working in Issue 51

--- trunk/src/customMsgBox.py
+++ trunk/src/customMsgBox.py
@@ -1,11 +1,13 @@
 ## @package src.customMsgBox
-# A custom message box widget.
+# A group of custom message box widgets.
 
 import pygtk
 pygtk.require('2.0')
 import gtk
+import atk
 
 
+## Message used to display errors
 def error_msg(parent, strMessage, buttons=gtk.BUTTONS_OK):
     dialog = gtk.MessageDialog(parent,
             gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
@@ -14,10 +16,25 @@ def error_msg(parent, strMessage, buttons=gtk.BUTTONS_OK):
     dialog.destroy()
     return resp
 
+def hyperlink(strUrl):
+    label_URL = gtk.Label()
+    label_URL.set_text("<span foreground=\"blue\" underline=\"single\">" +
+                    strUrl +  "</span>")
+    label_URL.set_use_markup(True)
+    return label_URL
+
+
 ## Message used in the updated notifications
 class updateMsgBox(gtk.Window):
-    def __init__(self, strMessage, strUrl, strDownloadUrl):
+    def btn_ok():
+        button = gtk.Button(stock=gtk.STOCK_OK)
+        button.connect("clicked", lambda *w: gtk.main_quit())
+        hbox = gtk.HButtonBox()
+        hbox.pack_start(button)
+        hbox.set_layout(gtk.BUTTONBOX_SPREAD)
+        return hbox
 
+    def __init__(self, strMessage, strUrl, strDownloadUrl):
         gtk.Window.__init__(self)
         self.connect("destroy", lambda *w: gtk.main_quit())
         self.set_border_width(10)
@@ -29,6 +46,7 @@ class updateMsgBox(gtk.Window):
 
         vbox = gtk.VBox(False)
         vbox.pack_start(hbox)
-        vbox.pack_start(gtk.Label(strUrl))
+        vbox.pack_start(hyperlink(strUrl))
+        vbox.pack_start(self.btn_ok())
         self.add(vbox)
         self.show_all()
