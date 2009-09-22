bf0faaa393a03c47f0ccb239e52b841cf4a602d0
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-09-22 20:52:11 +0000

    Improve the build of tar.gz file

--- trunk/installer/build_gz.sh
+++ trunk/installer/build_gz.sh
@@ -2,13 +2,19 @@
 # This Script is used to build the tar.gz file
 
 cd ..
+# Clean up temporary files
+find . -name \*.pyc | xargs rm
+find . -name \*.py~ | xargs rm
 
+# Get the Name 
 dName="`grep -w "NAME = " src/mapConst.py`"
 dName=${dName:8}
 dName=${dName/%\"}
 
+# Get the Version
 dVer="`grep -w "VERSION = " src/mapConst.py`"
 dVer=${dVer:11}
 dVer=${dVer/%\"}
 
+# Create the tar.gz file
 tar czf ../$dName-$dVer.tar.gz *.py src images installer Changelog README
