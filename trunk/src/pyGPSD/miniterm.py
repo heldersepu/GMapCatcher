cc167f7e4a4a8560f1300a21f4f27b8ad2ead9d8
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-09-22 02:08:28 +0000

    further simplify

--- trunk/src/pyGPSD/miniterm.py
+++ trunk/src/pyGPSD/miniterm.py
@@ -8,7 +8,7 @@
 # repr, useful for debug purposes)
 
 
-import sys, os, threading
+import sys, threading
 import nmea.serial as serial
 
 
@@ -16,91 +16,10 @@ DEFAULT_PORT = 2
 DEFAULT_BAUD = 4800
 
 
-def key_description(character):
-    """generate a readable description for a key"""
-    ascii_code = ord(character)
-    if ascii_code < 32:
-        return 'Ctrl+%c' % (ord('@') + ascii_code)
-    else:
-        return repr(character)
-
-
-# first choose a platform dependant way to read single characters from the console
-global console
-
-if os.name == 'nt':
-    import msvcrt
-    class Console:
-        def __init__(self):
-            pass
-
-        def setup(self):
-            pass    # Do nothing for 'nt'
-
-        def cleanup(self):
-            pass    # Do nothing for 'nt'
-
-        def getkey(self):
-            while 1:
-                z = msvcrt.getch()
-                if z == '\0' or z == '\xe0':    #functions keys
-                    msvcrt.getch()
-                else:
-                    if z == '\r':
-                        return '\n'
-                    return z
-
-    console = Console()
-
-elif os.name == 'posix':
-    import termios, sys, os
-    class Console:
-        def __init__(self):
-            self.fd = sys.stdin.fileno()
-
-        def setup(self):
-            self.old = termios.tcgetattr(self.fd)
-            new = termios.tcgetattr(self.fd)
-            new[3] = new[3] & ~termios.ICANON & ~termios.ECHO & ~termios.ISIG
-            new[6][termios.VMIN] = 1
-            new[6][termios.VTIME] = 0
-            termios.tcsetattr(self.fd, termios.TCSANOW, new)
-            #s = ''    # We'll save the characters typed and add them to the pool.
-
-        def getkey(self):
-            c = os.read(self.fd, 1)
-            return c
-
-        def cleanup(self):
-            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old)
-
-    console = Console()
-
-    def cleanup_console():
-        console.cleanup()
-
-    console.setup()
-    sys.exitfunc = cleanup_console      #terminal modes have to be restored on exit...
-
-else:
-    raise "Sorry no implementation for your platform (%s) available." % sys.platform
-
-
-CONVERT_CRLF = 2
-CONVERT_CR   = 1
-CONVERT_LF   = 0
-NEWLINE_CONVERISON_MAP = ('\n', '\r', '\r\n')
-LF_MODES = ('LF', 'CR', 'CR/LF')
-
-REPR_MODES = ('raw', 'some control', 'all control', 'hex')
-
 class Miniterm:
-    def __init__(self, port, baudrate, parity, rtscts, xonxoff, echo=False, convert_outgoing=CONVERT_CRLF, repr_mode=0):
+    def __init__(self, port, baudrate, parity, rtscts, xonxoff, echo=False):
         self.serial = serial.serial_for_url(port, baudrate, parity=parity, rtscts=rtscts, xonxoff=xonxoff, timeout=1)
         self.echo = echo
-        self.repr_mode = repr_mode
-        self.convert_outgoing = convert_outgoing
-        self.newline = NEWLINE_CONVERISON_MAP[self.convert_outgoing]
         self.dtr_state = True
         self.rts_state = True
         self.break_state = False
@@ -115,41 +34,14 @@ class Miniterm:
     def stop(self):
         self.alive = False
 
-    def join(self, transmit_only=False):
-        if not transmit_only:
-            self.receiver_thread.join()
+    def join(self):
+        self.receiver_thread.join()
 
     def reader(self):
         """loop and copy serial->console"""
         while self.alive:
             data = self.serial.read(1)
-
-            if self.repr_mode == 0:
-                # direct output, just have to care about newline setting
-                if data == '\r' and self.convert_outgoing == CONVERT_CR:
-                    sys.stdout.write('\n')
-                else:
-                    sys.stdout.write(data)
-            elif self.repr_mode == 1:
-                # escape non-printable, let pass newlines
-                if self.convert_outgoing == CONVERT_CRLF and data in '\r\n':
-                    if data == '\n':
-                        sys.stdout.write('\n')
-                    elif data == '\r':
-                        pass
-                elif data == '\n' and self.convert_outgoing == CONVERT_LF:
-                    sys.stdout.write('\n')
-                elif data == '\r' and self.convert_outgoing == CONVERT_CR:
-                    sys.stdout.write('\n')
-                else:
-                    sys.stdout.write(repr(data)[1:-1])
-            elif self.repr_mode == 2:
-                # escape all non-printable, including newline
-                sys.stdout.write(repr(data)[1:-1])
-            elif self.repr_mode == 3:
-                # escape everything (hexdump)
-                for character in data:
-                    sys.stdout.write("%s " % character.encode('hex'))
+            sys.stdout.write(data)            
             sys.stdout.flush()
 
 
@@ -159,11 +51,10 @@ def main():
             rtscts=False, xonxoff=False,
         )
     except serial.SerialException, e:
-        sys.stderr.write("could not open port %r: %s\n" % (port, e))
+        sys.stderr.write("could not open port %r: %s\n" % (DEFAULT_PORT, e))
         sys.exit(1)
 
     miniterm.start()
-    miniterm.join(True)
     miniterm.join()
 
 
