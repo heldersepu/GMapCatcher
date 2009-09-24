b42f40281c6ccc6a3d74519847148e2b87cc010a
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-09-24 00:53:38 +0000

    modify the Scan to also test for valid NMEA sentences

--- trunk/src/pyGPSD/scan.py
+++ trunk/src/pyGPSD/scan.py
@@ -1,24 +1,40 @@
 #! /usr/bin/env python
-"""\
-Scan for serial ports.
 
-Part of pySerial (http://pyserial.sf.net)
-(C) 2002-2003 <cliechti@gmx.net>
+# Scan for serial ports.
 
-The scan function of this module tries to open each port number
-from 0 to 255 and it builds a list of those ports where this was
-successful.
-"""
+# Part of pySerial (http://pyserial.sf.net)
+# (C) 2002-2003 <cliechti@gmx.net>
+
+# The scan function of this module tries to open each port number
+# from 0 to 255 and it builds a list of those ports where this was
+# successful.
+
 
 import nmea.serial as serial
 
-def scan():
-    """scan for available ports. return a list of tuples (num, name)"""
+DEFAULT_BAUD = 4800
+
+## Scan for available ports. return a list of tuples (num, name, NMEA)
+def scan(baudrate):
     available = []
     for i in range(256):
         try:
-            s = serial.Serial(i)
-            available.append( (i, s.portstr))
+            s = serial.Serial(i, baudrate)
+            intCount = 0
+            # Test if the port has an NMEA device attached
+            # NMEA sentence can be as long as 82 characters
+            # NMEA sentence start with '$GP'
+            NMEA = '$GP'
+            for j in range(90):
+                byte = s.read(1)
+                if byte == NMEA[intCount]:
+                    intCount += 1
+                    if intCount == 3:
+                        break
+                else:
+                    intCount = 0
+
+            available.append((i, s.portstr, intCount == 3))
             s.close()   # explicit close 'cause of delayed GC in java
         except serial.SerialException:
             pass
@@ -26,5 +42,7 @@ def scan():
 
 if __name__=='__main__':
     print "Found ports:"
-    for n,s in scan():
-        print "(%d) %s" % (n,s)
+    print "  #,  Name,  NMEA"
+    print "--------------------"
+    for n,s,b in scan(DEFAULT_BAUD):
+        print "  %d,  %s,  %s" % (n,s,b)
