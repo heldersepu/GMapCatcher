f0b5f7dfbbcf4d97b98611832a3696c74de1fec2
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-09-24 03:31:22 +0000

    Change the scan into a class and lunch the nmea_test in a thread with a 5 sec timeout

--- src/pyGPSD/scan.py
+++ src/pyGPSD/scan.py
@@ -11,38 +11,54 @@
 
 
 import nmea.serial as serial
+import threading
 
 DEFAULT_BAUD = 4800
 
-## Scan for available ports. return a list of tuples (num, name, NMEA)
-def scan(baudrate):
-    available = []
-    for i in range(256):
-        try:
-            s = serial.Serial(i, baudrate)
-            intCount = 0
-            # Test if the port has an NMEA device attached
-            # NMEA sentence can be as long as 82 characters
-            # NMEA sentence start with '$GP'
-            NMEA = '$GP'
-            for j in range(90):
-                byte = s.read(1)
-                if byte == NMEA[intCount]:
-                    intCount += 1
-                    if intCount == 3:
-                        break
-                else:
-                    intCount = 0
+## Scan for available ports.
+class PortScan:
+    ## Test if the port has an NMEA device attached
+    # NMEA sentence can be as long as 82 characters
+    # NMEA sentence start with '$GP'
+    def nmea_test(self, serial_port):
+        NMEA = '$GP'
+        intCount = 0
+        for j in range(90):
+            byte = serial_port.read(1)
+            if byte == NMEA[intCount]:
+                intCount += 1
+                if intCount == 3:
+                    break
+            else:
+                intCount = 0
+        self.isNMEA = intCount == 3
 
-            available.append((i, s.portstr, intCount == 3))
-            s.close()   # explicit close 'cause of delayed GC in java
-        except serial.SerialException:
-            pass
-    return available
+    ## Initialise the scan
+    def __init__(self, baudrate):
+        self.available = []
+        for i in range(256):
+            try:
+                self.isNMEA = False
+                s = serial.Serial(i, baudrate)
+                # Start the nmea_test in a thread with a 5 second timeout
+                myThread = threading.Thread(self.nmea_test(s))
+                myThread.setDaemon(1)
+                myThread.start()
+                myThread.join(5)
+                self.available.append((i, s.portstr, self.isNMEA))
+                s.close()   # explicit close 'cause of delayed GC in java
+            except serial.SerialException:
+                pass
+
+    ## returns a list of tuples (num, name, NMEA)
+    def found(self):
+        return self.available
 
 if __name__=='__main__':
+    port_scan = PortScan(DEFAULT_BAUD)
     print "Found ports:"
     print "  #,  Name,  NMEA"
     print "--------------------"
-    for n,s,b in scan(DEFAULT_BAUD):
+    for n,s,b in port_scan.found():
         print "  %d,  %s,  %s" % (n,s,b)
+    print "+------------------+"
