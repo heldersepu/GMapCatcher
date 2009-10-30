56f191b5577b258964bcf56f673cae273a4e250e
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-10-30 02:14:49 +0000

    Fix googleMapMaker.py

--- trunk/src/googleMapMaker.py
+++ trunk/src/googleMapMaker.py
@@ -1,12 +1,35 @@
 ## @package src.googleMapMaker
 # All the interaction with Google Map Maker
 
+import re
+import openanything
 from mapConst import MAP_MAX_ZOOM_LEVEL
 
+known_layers = {}
+
 ## Returns a template URL for the Google Map Maker
-def layer_url_template():    
-    return 'http://gt%d.google.com/mt/n=404&v=gwm.1168&x=%i&y=%i&zoom=%i'
+def layer_url_template(layer):
+    if layer not in known_layers:
+        known_layers[layer] = 'http://gt%d.google.com/mt/n=404&v=gwm.' + \
+                                parse_page() + '&x=%i&y=%i&zoom=%i'
+    return known_layers[layer]
 
 ## Returns the URL to the Google Map Maker tile
 def get_url(counter, coord):
-    return layer_url_template() % (counter, coord[0], coord[1], coord[2]+1)
+    return layer_url_template('gwm') % \
+        (counter, coord[0], coord[1], coord[2]+1)
+
+## Parse http://www.google.com/mapmaker.
+#  the return value is a number
+def parse_page():
+    gwmNum = '1170'
+    oa = openanything.fetch('http://www.google.com/mapmaker')
+    if oa['status'] != 200:
+        print "Trying to fetch Google Map Maker but failed"
+    else:
+        html = oa['data']
+        p = re.compile('gwm.([0-9]+)')
+        m = p.search(html)
+        if m:
+            gwmNum = m.group(1)
+    return gwmNum
