e2a5bc46fc2594c4a38e89b7cabf459ff377e80a
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-12-23 21:51:23 +0000

    some adjustments in virtualEarth.py

--- trunk/src/mapServers/virtualEarth.py
+++ trunk/src/mapServers/virtualEarth.py
@@ -10,15 +10,14 @@ def layer_url_template():
 ## Returns the URL to the virtualEarth tile
 def get_url(counter, coord):
     version = 392
-    print tile_to_quadkey(coord)
     return layer_url_template() % (counter, tile_to_quadkey(coord), version)
 
 def tile_to_quadkey(coord):
     quadKey = []
-    for num in xrange(coord[2], 0, -1):
+    for num in xrange(17 - coord[2], 0, -1):
         digit = 0
         mask = 1 << (num - 1)
-        if (coord[0] and mask) != 0:  digit += 1
-        if (coord[1] and mask) != 0:  digit += 2
+        if (coord[0] & mask) != 0:  digit += 1
+        if (coord[1] & mask) != 0:  digit += 2
         quadKey.append(`digit`)
     return ''.join(quadKey)
