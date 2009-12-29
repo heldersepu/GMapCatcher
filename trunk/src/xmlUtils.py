1aa23b401efcca9fb054d899b45d26ceda72e70d
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2009-12-29 03:09:09 +0000

    first step towards issue 65

---
+++ trunk/src/xmlUtils.py
@@ -0,0 +1,29 @@
+
+def import_kml(strFileName):
+    from xml.dom.minidom import parse
+
+    def getText(nodelist):mua
+        rc = ""
+        for node in nodelist:
+            if node.nodeType == node.TEXT_NODE:
+                rc = rc + node.data
+        return rc
+
+    dom = parse(strFileName)
+
+    PlacemarkElements = dom.getElementsByTagName("Placemark")
+
+    for element in PlacemarkElements:
+        try:
+            NameElement = element.getElementsByTagName("name")[0]
+            PointElement = element.getElementsByTagName("Point")[0]
+            CoordElement = PointElement.getElementsByTagName("coordinates")[0]
+        except:
+            pass
+        else:
+            print getText(NameElement.childNodes)
+            print getText(CoordElement.childNodes)
+            print " "
+
+if __name__ == "__main__":
+    import_kml('KML_Samples.kml')
