cb2b25f7beeb6883fbf4fbb8907686d6890cd571
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2010-01-06 13:51:36 +0000

    Create an experimental branch

---
+++ branches/alex/src/mapServers/openCycleMap.py
@@ -0,0 +1,15 @@
+## @package src.mapServers.openCycleMap
+# All the interaction with OpenCycleMap.org
+
+from src.mapConst import MAP_MAX_ZOOM_LEVEL
+
+## Returns a template URL for the OpenCycleMap
+def layer_url_template():
+    
+    return 'http://%s.andy.sandbox.cloudmade.com/tiles/cycle/%i/%i/%i.png'
+
+## Returns the URL to the OpenCycleMap tile
+def get_url(counter, coord):
+    server = ['a', 'b', 'c']
+    return layer_url_template() % (server[counter % 3], 
+                MAP_MAX_ZOOM_LEVEL - coord[2], coord[0], coord[1])
