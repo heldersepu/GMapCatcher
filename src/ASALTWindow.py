140fef4c3d43f9462deca0b850855bcdad080592
sirknottalot <sirknottalot@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2010-01-22 23:48:56 +0000

    added console, need to create function to add text to the window

--- src/ASALTWindow.py
+++ src/ASALTWindow.py
@@ -16,28 +16,13 @@ from os.path import join, isdir
 
 class ASALTWindow(gtk.Window):
 
+
+
     def __init__(self, layer, init_path, mapServ, styleID):
 
-        def _zoom(zoom0, zoom1):
-            out_hbox = gtk.HBox(False, 50)
-            out_hbox.set_border_width(10)
-            in_hbox = gtk.HBox(False, 20)
-            in_hbox.pack_start(lbl("min:"), False)
-            self.s_zoom0 = _SpinBtn(zoom0)
-            self.s_zoom0.set_digits(0)
-            in_hbox.pack_start(self.s_zoom0)
-            out_hbox.pack_start(in_hbox)
+        def txt_to_console(self, text):
+            #code to insert text to console
 
-            in_hbox = gtk.HBox(False, 20)
-            in_hbox.pack_start(lbl("max:"), False)
-            self.s_zoom1 = _SpinBtn(zoom1)
-            self.s_zoom1.set_digits(0)
-            in_hbox.pack_start(self.s_zoom1)
-            out_hbox.pack_start(in_hbox)
-            hbox = gtk.HBox()
-            hbox.set_border_width(10)
-            hbox.pack_start(_frame(" Zoom ", out_hbox, 0))
-            return hbox
 
         def _console():
             vbox = gtk.VBox(False, 5)
@@ -56,7 +41,10 @@ class ASALTWindow(gtk.Window):
             sw = gtk.ScrolledWindow()
 	    sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 	    textview = gtk.TextView()
+	    textview.set_editable(False)
 	    textbuffer = textview.get_buffer()
+	    textbuffer.insert_at_cursor("AKFYAH!\n")
+	    txt_to_console(self, "foobar")
 	    sw.add(textview)
 	    sw.show()
 	    textview.show()
@@ -68,21 +56,7 @@ class ASALTWindow(gtk.Window):
             
             return _frame(" Console ", vbox)
 
-        def _area():
-            vbox = gtk.VBox(False, 5)
-            hbox = gtk.HBox(False, 10)
-            hbox.pack_start(lbl("width:"))
-            #self.e_kmx = _myEntry("%.6g" % kmx, 10, False)
-            #hbox.pack_start(self.e_kmx, False)
-            vbox.pack_start(hbox)
-
-            hbox = gtk.HBox(False, 10)
-            hbox.pack_start(lbl("height:"))
-            #self.e_kmy = _myEntry("%.6g" % kmy, 10, False)
-            #hbox.pack_start(self.e_kmy, False)
-            vbox.pack_start(hbox)
-            return _frame(" Area (km) ", vbox)
-
+   
         def _buttons(strFolder):
             hbbox = gtk.HButtonBox()
             hbbox.set_border_width(10)
