4383b6f1de647a8b0381920260f1372fa0a2bf3e
sirknottalot <sirknottalot@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2010-02-19 05:14:52 +0000

    improved send function for unique message to vehicle

--- src/ASALTradio.py
+++ src/ASALTradio.py
@@ -2,16 +2,40 @@
 # Widget that for the ASALT vehicle
 
 import pyGPSD.nmea.serial as serial
-
+import time
 
 
 
 
 class ASALTradio():
 
-    def send(self, str):
-       coord1 = "%3.10f" % str[0]
-       coord2 = "%3.10f" % str[1]
+    def send(self, data, type):
+       start_char = struct.pack(c,\xAA)
+       switch(type){
+           case '1':
+              print "send coordinate type"
+              size = data[1]
+              self.ser.write(start_char)
+       	      self.ser.write(struct.pack(c,'1'))
+       	      self.ser.write(struct.pack(c,data[0]))
+       	      self.ser.write(struct.pack(c,size))
+       	      for coord in data[2:size+2]:
+       	          self.ser.write(struct.pack(d,coord[0]))
+       	          self.ser.write(struct.pack(d,coord[1]))
+       	          self.ser.write(struct.pack(I,coord[2]))
+       	          time.sleep(0.1)
+       	          
+       	   case '2':	      
+       	      self.ser.write(start_char)
+       	      self.ser.write(struct.pack(c,'2'))
+       	   default:
+       	      print "ASALTRadio send: invalid type"
+       }
+    
+    
+    
+       coord1 = "%3.10f" % data[0]
+       coord2 = "%3.10f" % data[1]
        if(self.ser.isOpen()):
           self.ser.write(coord1)
           self.ser.write(" ")
@@ -22,12 +46,15 @@ class ASALTradio():
     def receive(self):
        
 
+    def query(self):
+        send(NULL, '2')
 
-	def __init__(self):
-		self.ser = serial.Serial()
-		self.ser.baudrate = 57600
-		self.ser.port = 0 
-		self.ser.open()
+
+    def __init__(self):
+	self.ser = serial.Serial()
+	self.ser.baudrate = 19200
+	self.ser.port = 0 
+	self.ser.open()
 
     
     
