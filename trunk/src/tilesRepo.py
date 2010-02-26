0b82903df72f389fa8b5679ba8e8d817708a92da
standa31415 <standa31415@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2010-02-26 22:35:43 +0000

    Issue 72 - Sorry, missing class was added

---
+++ trunk/src/tilesRepo.py
@@ -0,0 +1,41 @@
+## This modul provides filebased tile repository functions
+#
+# Usage:
+#
+# base class for tile repository
+#
+# - constructor requires MapServ instance, because method
+#  'get_tile_from_coord' is provided in the MapServ
+#
+# - this module is not used directly. It is used via MapServ() methods:
+#     - get_file()
+#     - load_pixbuf()
+# - module is finalized from MapServ.finish() method
+
+
+class NotImplementedException(Exception):
+    pass
+
+class TilesRepository:
+
+    def __init__(self, MapServ_inst):
+        pass
+
+    def finish(self):
+        raise NotImplementedException()
+
+    def load_pixbuf(self, coord, layer, force_update):
+        raise NotImplementedException()
+    
+    def get_tile(self, tcoord, layer, online, force_update, mapServ, styleID):
+        raise NotImplementedException()
+    
+    def do_export(self, tcoord, layer, online, mapServ, styleID, size):
+        raise NotImplementedException()
+    
+    def remove_old_tile(self, coord, layer):
+        raise NotImplementedException()
+
+    def is_tile_in_local_repos(self, coord, layer, intSeconds=86400):
+        raise NotImplementedException()
+
