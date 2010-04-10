c0f19dd8df88599373cf5d6eddd119adab897921
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2010-04-10 02:13:52 +0000

    revert to prior revision (undo Standa changes r764)

--- trunk/src/mapServers/yahoo.py
+++ trunk/src/mapServers/yahoo.py
@@ -1,11 +1,11 @@
 ## @package src.mapServers.yahoo
 # All the interaction with yahoo.com
 
-from src.mapConst import MAP_MAX_ZOOM_LEVEL, MAP_SERVICES
+from src.mapConst import MAP_MAX_ZOOM_LEVEL, LAYER_MAP
 
 ## Returns a template URL for the Yahoo mas
 def layer_url_template(layer):
-    if MAP_SERVICES[layer]["TextID"] == "ymap": #LAYER_MAP:
+    if layer == LAYER_MAP:
         return 'http://maps.yimg.com/hw/tile?&v=9&imgtype=png&s=256&x=%i&y=%i&z=%i'
     else:
         return 'http://maps.yimg.com/ae/ximg?v=9&t=s&imgtype=png&s=256&x=%i&y=%i&z=%i'
