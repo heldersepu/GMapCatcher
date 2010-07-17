e43a1369bfd5b00de346a8b497a190254bc1dd6b
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2010-07-17 20:34:41 +0000

    Now I know transparency!

--- gmapcatcher/mapPixbuf.py
+++ gmapcatcher/mapPixbuf.py
@@ -12,7 +12,7 @@ if 'library.zip' in __file__:
     _prefix = abspath(join(dirname(__file__), "../../images"))
 else:
     _prefix = abspath(join(dirname(__file__), "../images"))
-    
+
 if not isfile(join(_prefix, 'missing.png')):
     _prefix = '/usr/share/pixmaps/gmapcatcher'
 
@@ -28,29 +28,29 @@ def missing():
     try:
         pix_missing = gtk.gdk.pixbuf_new_from_file(join(_prefix, 'missing.png'))
     except Exception:
-        pix_missing = gtk.gdk.pixbuf_new_from_data('\255\255\255' * 100000,
+        pix_missing = gtk.gdk.pixbuf_new_from_data(
+            ('\0\0\0' + '\255\255\255' * 3) * (TILES_WIDTH / 4) + 
+            (('\0\0\0' + '\255\255\255' * (TILES_WIDTH - 1)) +
+             ('\255\255\255' * 3 * TILES_WIDTH)) * (TILES_HEIGHT / 4),              
             gtk.gdk.COLORSPACE_RGB, False, 8,
             TILES_WIDTH, TILES_HEIGHT, TILES_HEIGHT * 3)
     return pix_missing
 
-## Get the Pixbuf from cross.png
+## Get the Pixbuf of a Cross
 def cross():
-    try:
-        pix_cross = gtk.gdk.pixbuf_new_from_file(join(_prefix, 'cross.png'))
-    except Exception:
-        pix_cross = gtk.gdk.pixbuf_new_from_data(
-            ('\255\255\255' * 4 + '\0\0\255' * 4 + '\255\255\255' * 4) * 4 +
-            ('\0\0\255' * 12) * 4 +
-            ('\255\255\255' * 4 + '\0\0\255' * 4 + '\255\255\255' * 4) * 4,
-            gtk.gdk.COLORSPACE_RGB, False, 8, 12, 12, 12 * 3)
-    return pix_cross
-    
+    return gtk.gdk.pixbuf_new_from_data(
+            ('\0\0\0\0' * 5 + '\0\0\255\350' * 2 + '\0\0\0\0' * 5) * 5 +
+            ('\0\0\255\350' * 12) * 2 +
+            ('\0\0\0\0' * 5 + '\0\0\255\350' * 2 + '\0\0\0\0' * 5) * 5,
+            gtk.gdk.COLORSPACE_RGB, True, 8, 12, 12, 12 * 4)
+
 def downloading():
     try:
         pix_dl = gtk.gdk.pixbuf_new_from_file(join(_prefix, 'downloading.png'))
     except Exception:
         pix_dl = gtk.gdk.pixbuf_new_from_data(
-            '\0\255\0\127' * 600, gtk.gdk.COLORSPACE_RGB, True, 8, 50, 12, 50 * 4)
+            '\0\255\0\127' * 150 * 38, 
+            gtk.gdk.COLORSPACE_RGB, True, 8, 150, 38, 150 * 4)
     return pix_dl
 
 ## Get the Pixbuf from the given image.
@@ -66,8 +66,8 @@ def getImage(filename, intWidth=56, intHeight=56):
         hCo = int(intHeight / 3)
         hMe = intHeight - 2 * wCo
         pix_buf = gtk.gdk.pixbuf_new_from_data(
-            ('\255\0\0' * wCo + '\0\0\255' * wMe + '\255\0\0' * wCo) * hCo +
-            ('\0\0\255' * intWidth) * hMe +
-            ('\255\0\0' * wCo + '\0\0\255' * wMe + '\255\0\0' * wCo) * hCo,
-            gtk.gdk.COLORSPACE_RGB, False, 8, intWidth, intHeight, intHeight * 3)
+            ('\255\0\0\127' * wCo + '\0\0\255\127' * wMe + '\255\0\0\127' * wCo) * hCo +
+            ('\0\0\255\127' * intWidth) * hMe +
+            ('\255\0\0\127' * wCo + '\0\0\255\127' * wMe + '\255\0\0\127' * wCo) * hCo,
+            gtk.gdk.COLORSPACE_RGB, True, 8, intWidth, intHeight, intHeight * 4)
     return pix_buf
