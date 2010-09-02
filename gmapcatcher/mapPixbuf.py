675cb649d900ab75087e5eb49efd19243ba1b55b
Mark111111 <Mark111111@gmail.com>
2010-09-02 09:57:46 +0000

    simplify gtk identification in mapPixbuf

--- gmapcatcher/mapPixbuf.py
+++ gmapcatcher/mapPixbuf.py
@@ -4,7 +4,8 @@
 #
 
 import os
-if os.environ.get('MAPS_GTK', "False") == "True":
+IS_GTK = os.environ.get('MAPS_GTK', "False") == "True"
+if IS_GTK:
     import gtk
 from mapConst import *
 from os.path import join, dirname, abspath, exists, isfile
@@ -17,6 +18,8 @@ else:
 
 if not isfile(join(_prefix, 'missing.png')):
     _prefix = '/usr/share/pixmaps/gmapcatcher'
+    
+
 
 def ico():
     pix_ico = False
@@ -54,7 +57,7 @@ def getImage(filename, intWidth=56, intHeight=56):
     try:
         if not exists(filename):
             filename = join(_prefix, filename)
-        if os.environ.get('MAPS_GTK', 'False') == 'True':
+        if IS_GTK:
             pix_buf = gtk.gdk.pixbuf_new_from_file_at_size(
                     filename, intWidth, intHeight)
     except Exception:
@@ -62,7 +65,7 @@ def getImage(filename, intWidth=56, intHeight=56):
         wMe = intWidth - 2 * wCo
         hCo = int(intHeight / 3)
         hMe = intHeight - 2 * wCo
-        if os.environ.get('MAPS_GTK', 'False') == 'True':
+        if IS_GTK:
             pix_buf = gtk.gdk.pixbuf_new_from_data(
                     ('\255\0\0\127' * wCo + '\0\0\255\127' *
                     wMe + '\255\0\0\127' * wCo) * hCo +
@@ -74,7 +77,7 @@ def getImage(filename, intWidth=56, intHeight=56):
     return pix_buf
     
 def image_data_fs(filename):
-    if os.environ.get('MAPS_GTK', 'False') == 'True':
+    if IS_GTK:
         return gtk.gdk.pixbuf_new_from_file(filename)
     else:
         thefile = open(filename, 'rb')
@@ -83,7 +86,7 @@ def image_data_fs(filename):
         return ret
 
 def image_data_direct(name):
-    if os.environ.get('MAPS_GTK', 'False') == 'True':
+    if IS_GTK:
         if name == "missing":
             return gtk.gdk.pixbuf_new_from_data(
                     ('\0\0\0' + '\255\255\255' * 3) * (TILES_WIDTH / 4) + 
