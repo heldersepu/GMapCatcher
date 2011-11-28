734d28cef1258825b1f62c2ad80b14fa3e803f4c
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2011-11-28 15:40:18 +0000

    combine 2 scripts in 1

--- wmap/js/map.js
+++ wmap/js/map.js
@@ -1,21 +1,37 @@
+OpenLayers.Util.OSM = {};
+OpenLayers.Util.OSM.MISSING_TILE_URL = "http://www.openstreetmap.org/openlayers/img/404.png";
+OpenLayers.Util.OSM.originalOnImageLoadError = OpenLayers.Util.onImageLoadError;
+OpenLayers.Util.onImageLoadError = function() {
+    this.src = OpenLayers.Util.OSM.MISSING_TILE_URL;
+};
+
+OpenLayers.Layer.OSM.Map = OpenLayers.Class(OpenLayers.Layer.OSM, {
+    initialize: function(name, options) {
+        var url = ["http://127.0.0.1/tiles/${z}/0/${x}/0/${y}.png"];
+        options = OpenLayers.Util.extend({
+            numZoomLevels: 19, buffer: 0, transitionEffect: "resize"
+        }, options);
+        var newArg = [name, url, options];
+        OpenLayers.Layer.OSM.prototype.initialize.apply(this, newArg);
+    }, CLASS_NAME: "OpenLayers.Layer.OSM.Map"
+});
+
 function mapInit(lat, lon, zoom) {
 	map = new OpenLayers.Map("theMap", {
 		controls:[new OpenLayers.Control.Navigation()],
 			maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
-			maxResolution: 156543.0399,
-			numZoomLevels: 19,
-			units: 'm',
+			maxResolution: 156543.0399, numZoomLevels: 19, units: 'm',
 		projection: new OpenLayers.Projection("EPSG:900913"),
 		displayProjection: new OpenLayers.Projection("EPSG:4326")
 	} );
-	osmAddMap_Layers();
+	addMap_Layers();
 	osmMapCenter(lat, lon, zoom);
 }
 
 // Define the map layers
-function osmAddMap_Layers() {
-	layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
-	map.addLayer(layerMapnik);
+function addMap_Layers() {
+	layerMap = new OpenLayers.Layer.OSM.Map("Map");
+	map.addLayer(layerMap);
 }
 
 // Move the center of the map to the given coordinates
