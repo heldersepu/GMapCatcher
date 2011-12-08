736c776409e856e6b7c62c22a9c44e3f062edfc9
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2011-12-08 16:13:04 +0000

    Add multiple layers and use dyndns

--- wmap/js/map.js
+++ wmap/js/map.js
@@ -1,5 +1,5 @@
 OpenLayers.Util.OSM = {};
-OpenLayers.Util.OSM.MISSING_TILE_URL = "http://127.0.0.1/tiles/404.png";
+OpenLayers.Util.OSM.MISSING_TILE_URL = "http://ipmia.dyndns.org/img/404.png";
 OpenLayers.Util.OSM.originalOnImageLoadError = OpenLayers.Util.onImageLoadError;
 OpenLayers.Util.onImageLoadError = function() {
 	this.src = OpenLayers.Util.OSM.MISSING_TILE_URL;
@@ -7,17 +7,28 @@ OpenLayers.Util.onImageLoadError = function() {
 
 OpenLayers.Layer.OSM.Map = OpenLayers.Class(OpenLayers.Layer.OSM, {
 	initialize: function(name, opt) {
-		var url = ["http://127.0.0.1/tiles/${z}/${x}/${y}.png"];
-		opt = OpenLayers.Util.extend({numZoomLevels:19, buffer:0}, opt);
+		var url = ["http://ipmia.dyndns.org/tiles/${z}/${x}/${y}.png"];
+		opt = OpenLayers.Util.extend({numZoomLevels:19, buffer:0, transitionEffect: "resize"}, opt);
 		var newArg = [name, url, opt];
 		OpenLayers.Layer.OSM.prototype.initialize.apply(this, newArg);
 	}, CLASS_NAME: "OpenLayers.Layer.OSM.Map"
+});
+OpenLayers.Layer.OSM.Ter = OpenLayers.Class(OpenLayers.Layer.OSM, {
+	initialize: function(name, opt) {
+		var url = ["http://ipmia.dyndns.org/ter_tiles/${z}/${x}/${y}.png"];
+		opt = OpenLayers.Util.extend({numZoomLevels:19, buffer:0, transitionEffect: "resize"}, opt);
+		var newArg = [name, url, opt];
+		OpenLayers.Layer.OSM.prototype.initialize.apply(this, newArg);
+	}, CLASS_NAME: "OpenLayers.Layer.OSM.Ter"
 });
 
 function mapInit(divName, lat, lon, zoom) {
 	b = 20037508.34
 	map = new OpenLayers.Map(divName, {
-		controls:[new OpenLayers.Control.Navigation()],
+		controls:[
+				new OpenLayers.Control.Navigation(),
+                new OpenLayers.Control.PanZoomBar(),
+                new OpenLayers.Control.LayerSwitcher()],
 			maxExtent: new OpenLayers.Bounds(-b, -b, b, b),
 			maxResolution: 156543.0399, numZoomLevels: 19, units: 'm',
 		projection: new OpenLayers.Projection("EPSG:900913"),
@@ -29,8 +40,10 @@ function mapInit(divName, lat, lon, zoom) {
 
 // Define the map layers
 function addMap_Layers() {
-	layerMap = new OpenLayers.Layer.OSM.Map("Map");
+	layerMap = new OpenLayers.Layer.OSM.Map("my Map");
 	map.addLayer(layerMap);
+	layerTer = new OpenLayers.Layer.OSM.Ter("my Ter");
+	map.addLayer(layerTer);
 }
 
 // Move the center of the map to the given coordinates
