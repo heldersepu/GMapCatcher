c7d9aad2ceb8992cf8cda9e1e0112d98695231b2
heldersepu <heldersepu@c71b5c92-c342-0410-808e-5dff5bb07c7d>
2012-09-21 00:13:45 +0000

    Finalize... Move the code for the cmb_layer to its own package

--- trunk/gmapcatcher/widComboBoxLayer.py
+++ trunk/gmapcatcher/widComboBoxLayer.py
@@ -8,13 +8,15 @@ from gobject import TYPE_STRING
 
 
 ## This widget is where we the available Layers
-class ComboBoxLayer(gtk.ComboBox):
+class ComboBoxLayer(gtk.ComboBoxEntry):
 
     def __init__(self, conf, current_layer):
         super(ComboBoxLayer, self).__init__()
+        self.connect('key-press-event', self.key_press_combo)
+        self.child.connect('key-press-event', self.key_press_combo)
         self.conf = conf
-        self.current_layer = current_layer
         self.populate()
+        self.set_active(current_layer)
 
     def populate(self):
         store = gtk.ListStore(TYPE_STRING)
@@ -29,19 +31,23 @@ class ComboBoxLayer(gtk.ComboBox):
                     if kv['serviceName'] == self.conf.map_service and kv['ID'] == w:
                         iter = store.append()
                         store.set(iter, 0, LAYER_NAMES[w])
-        if (not self.conf.oneDirPerMap):
-            if self.current_layer in NON_ONEDIR_COMBO_INDICES[self.conf.map_service]:
-                self.set_active(
-                    NON_ONEDIR_COMBO_INDICES[self.conf.map_service]
-                    .index(self.current_layer))
-            else:
-                self.set_active(0)
-                self.layer_changed(self)
-        else:
-            self.set_active(self.current_layer)
+
         self.set_model(store)
-        self.set_entry_text_column(0)
+        self.set_text_column(0)
+    
+    ## Handles the pressing of keys    
+    def key_press_combo(self, w, event):
+        if event.keyval not in [65362, 65364]:
+            self.combo_popup()
+            return True
+
+    ## Show the combo list if is not empty
+    def combo_popup(self):
+        if self.get_model().get_iter_root() is not None:
+            self.popup()
 
     def refresh(self):
+        self.child.set_text('')
         self.set_model(None)
         self.populate()
+        self.combo_popup()
